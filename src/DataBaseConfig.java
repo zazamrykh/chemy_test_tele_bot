e2fb2a107367feb6770903a60bebaa0fb9fa0645
Zakhar Bastard <zaxar0.0.0@mail.ru>
2022-09-22 00:53:15 +0300

    Add .gitignore

--- src/DataBaseConfig.java
+++
@@ -1,8 +0,0 @@
-public class DataBaseConfig {
-    // DB structure https://dbdesign.online/model/Ubgce3iCYJfC
-    protected String dbHost = "localhost";
-    protected String dbPort = "3307";
-    protected String dbPassword = "hotdog88";
-    protected String dbName = "chemy_bot_schema";
-    protected String dbUser = "root";
-}
--- src/TelegramConfig.java
+++
@@ -1,4 +0,0 @@
-public class TelegramConfig {
-    protected static String botToken = "5561218392:AAFN6zkdajnnL-ufSlVONl0Fi2c0gD_Ekp0";
-    protected static String botUsername = "@ChemyTestBot";
-}
